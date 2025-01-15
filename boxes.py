b84f8a01e328daf18fe4e738cad8da4909f5c826
joakinleiva <mleiva@byupathway.edu>
2025-01-15 04:32:54 -0300

    Done with boxes

--- boxes.py
+++ boxes.py
@@ -1,40 +1,19 @@
 # #Boxes by Joaquin Leiva
+import math
 
-# import math
 from colorama import *
 init()
-# number_items = int(input("Insert the number of items: ")) # Asking for the number of items
-# print(" ") #Spacing
-# items_per_box = int(input("How many items you'd like to pack in each box?: ")) # Asking for the number of items per box
+
+number_items = int(input("Insert the number of items: ")) # Asking for the number of items
+print(" ") #Spacing
+items_per_box = int(input("How many items you'd like to pack in each box?: ")) # Asking for the number of items per box
 
 # #Function 
 
-# def itemsinbox(number_items, items_per_box):
-#     box_quantity = math.ceil(number_items / items_per_box)
+def itemsinbox(number_items, items_per_box):
+    box_quantity = math.ceil(number_items / items_per_box)
+    print(f"To place {number_items} items, packing {items_per_box} items in each box, you will need {box_quantity} boxes.")
     
-#     print(f"To place {number_items} items, packing {items_per_box} items in each box, you will need {box_quantity} boxes.")
-    
-# # Calling the function
+# Calling the function
 
-# itemsinbox(number_items, items_per_box)
-
-
-# import math
-
-# def calcular_cajas(items, items_por_caja):
-#   """Calcula el número de cajas necesarias."""
-#   return math.ceil(items / items_por_caja)
-
-# # Obtener datos del usuario
-# items = int(input("Ingrese el número de items: "))
-# items_por_caja = int(input("Ingrese el número de items por caja: "))
-
-# # Calcular y mostrar el resultado
-# resultado = calcular_cajas(items, items_por_caja)
-# print(f"Se necesitan {resultado} cajas.")
-
-print(Fore.RED + 'Este texto es rojo')
-print(" ")
-print(Back.GREEN + 'Este fondo es verde')
-print(" ")
-print(Style.BRIGHT + 'Texto brillante')
+itemsinbox(number_items, items_per_box)
